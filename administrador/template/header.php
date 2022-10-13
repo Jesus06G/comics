51f638d257bda11b6a29ae079f673e151fab0a14
ivan gutierrez <ivancuello92@gmail.com>
2022-10-13 09:17:02 -0500

    modificar admin

--- administrador/template/header.php
+++ administrador/template/header.php
@@ -32,18 +32,17 @@ else{
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-  <?php $url= 'https://' . $_SERVER ['HTTP_HOST']. '/sitioweb' ?>
  <nav class="navbar navbar-expand navbar-light bg-light">
      <div class="nav navbar-nav">
          <a class="nav-item nav-link active" href="#">Administrador del sitio web</a>
 
-         <a class="nav-item nav-link" href="<?php echo $url ."/administrador/inicio.php";?>">Inicio</a>
+         <a class="nav-item nav-link" href="administrador/inicio.php">Inicio</a>
 
-         <a class="nav-item nav-link" href="<?php echo $url ."/administrador/seccion/productos.php";?>">Administar libros</a>
+         <a class="nav-item nav-link" href="/administrador/seccion/productos.php">Administar libros</a>
 
-         <a class="nav-item nav-link" href="<?php echo $url ."/administrador/seccion/cerrar.php";?>">Cerrar sesion</a>
+         <a class="nav-item nav-link" href="administrador/seccion/cerrar.php">Cerrar sesion</a>
 
-         <a class="nav-item nav-link" href="<?php echo $url ?>">Ver sitio web</a>
+         <a class="nav-item nav-link" href="https://sitecomics-app.herokuapp.com/">Ver sitio web</a>
      </div>
  </nav>
 
