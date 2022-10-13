dd1e1b005d9eddea1d087b58117155983628112a
ivan gutierrez <ivancuello92@gmail.com>
2022-10-13 09:12:25 -0500

    conexion modificada

--- administrador/config/bd.php
+++ administrador/config/bd.php
@@ -1,9 +1,9 @@
 <?php 
 // Conexion a la base de datos.
-$host="localhost";
-$bd="libros";
-$usuario="root";
-$contrasenia="";
+$host="bpil1fpdcjy6d0cdccsl-mysql.services.clever-cloud.com";
+$bd="bpil1fpdcjy6d0cdccsl";
+$usuario="u8opfga0q4ahsh24";
+$contrasenia="htwMZONzxJhhMFpSA9AJ";
 
 try {
  $conexion= new PDO("mysql:host=$host;dbname=$bd",$usuario,$contrasenia);
